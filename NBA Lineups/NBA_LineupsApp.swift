9ffb64a8049db70778e1fdc8c55c1b59870776ed
Leiss Amini <leissamini03@gmail.com>
2024-12-11 09:35:55 -0800

    Initial Commit

---
+++ NBA Lineups/NBA_LineupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_LineupsApp.swift
+//  NBA Lineups
+//
+//  Created by Leiss Amini on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NBA_LineupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NBA LineupsTests/NBA_LineupsTests.swift
@@ -0,0 +1,16 @@
+//
+//  NBA_LineupsTests.swift
+//  NBA LineupsTests
+//
+//  Created by Leiss Amini on 12/11/24.
+//
+
+import Testing
+
+struct NBA_LineupsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
